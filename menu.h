11436c70e616e0393bd2ea5f3825f5ee495179e6
CHnChycal <15868758032@163.com>
2023-08-28 11:27:05 +0800

    re

---
+++ menu.h
@@ -0,0 +1,26 @@
+#ifndef MENU_H
+#define MENY_H
+#include "menu.cpp"
+class menu
+{
+private:
+    /* data */
+    int condition;
+public:
+    menu(/* args */);
+    ~menu();
+    void Gamestart(int i);
+    void Gamerunning();
+    
+};
+
+menu::menu()
+{
+}
+
+menu::~menu()
+{
+}
+
+
+#ifndef

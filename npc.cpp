924027f1ad9c133df875973d209bee59b1c2f780
梁应雄 <@LAPTOP-URN33A1Q>
2023-09-09 21:16:25 +0800

    123

--- npc.cpp
+++ npc.cpp
@@ -148,118 +148,129 @@ Npc::Npc(int Id) {
 }
 Npc::~Npc() {}
 
-void Npc::npctalk(int currentLocationIndex, Bag* bag) {
+void Npc::npctalk(int NpcId, Bag* bag) {
 	string text;
-	switch (currentLocationIndex) {
-	case 1: {
-		if (bag->showGoodNum(0) <= 0) {//看是否有徽章判断打败了npc没有
-			text = "铁旋:啊哈！你终于来了！我知道你想要什么。你想和我的宝可梦对战！哇哈哈哈哈！别说我没警告你，我会让你火花四溅的！";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
+	if (NpcId < 4) {
+		switch (NpcId) {
+		case 1: {
+			if (bag->showGoodNum(0) <= 0) {//看是否有徽章判断打败了npc没有
+				text = "铁旋:啊哈！你终于来了！我知道你想要什么。你想和我的宝可梦对战！哇哈哈哈哈！别说我没警告你，我会让你火花四溅的！";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+				cout << endl;
+				text = "你：哼！我才不怕你呢！让我们来比试比试吧！";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+				system("cls");
 			}
-			cout << endl;
-			text = "你：哼！我才不怕你呢！让我们来比试比试吧！";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
+			else {
+				text = "铁旋：我们又要给我们的电池充电了。等我们充好电，我们会很高兴接受你的挑战。所以，有时间的话再来吧，好吗？";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
 			}
-			system("cls");
+			break;
 		}
-		else {
-			text = "铁旋：我们又要给我们的电池充电了。等我们充好电，我们会很高兴接受你的挑战。所以，有时间的话再来吧，好吗？";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
+		case 2: {
+			if (bag->showGoodNum(1) <= 0) {
+				text = "亚莎：打输一次不会让我泄气。我爱宝可梦，我爱战斗，而且……我爱这间道馆！让我们用炙热的招式开战吧！";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+				cout << endl;
+				text = "你：听你这么说，你很勇哦？让我看看你的宝可梦有多强吧！";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+				system("cls");
 			}
+			else {
+				text = "亚莎：你的强大让我想起了一个人……和不同的人战斗，就能学到不同的人的优点！";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+			}
+			break;
 		}
-		break;
+		case 3: {
+			if (bag->showGoodNum(2) <= 0) {
+				text = "千里：嗯……看来，你已经赢得两枚徽章了。我真高兴能和自己的孩子来一场真真正正的战斗。但是，战斗就是战斗！身为道馆馆主，我会尽我所能赢取胜利。";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+				cout << endl;
+				text = "你：好的老爸！可别被我打得满地找牙！";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+				system("cls");
+			}
+			else {
+				text = "千里：你的梦想是什么？我的梦想……哈哈哈……已经实现了。";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+			}
+			break;
+		}
+		case 4: {
+			if (bag->showGoodNum(3) <= 0) {
+				text = "米可莉：请容我正式介绍我自己。我是米可莉，琉璃道馆的道馆馆主。现在，把你和你的宝可梦的力量展现给我看吧.";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+				cout << endl;
+				text = "你：请容我正式介绍我自己。我是秦始皇，我是来将你击败的！";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+				system("cls");
+			}
+			else {
+				text = "米可莉：太棒了！你真的是太棒了！你是一个真正的宝可梦训练家。我为能遇见你和你的宝可梦而喜悦。";
+				for (int i = 0; i < text.length(); i++)
+				{
+					Sleep(10);
+					cout << text[i];
+				}
+			}
+			break;
+		}
+			  break;
+		}
 	}
-	case 2: {
-		if (bag->showGoodNum(1) <= 0) {
-			text = "亚莎：打输一次不会让我泄气。我爱宝可梦，我爱战斗，而且……我爱这间道馆！让我们用炙热的招式开战吧！";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
-			}
-			cout << endl;
-			text = "你：听你这么说，你很勇哦？让我看看你的宝可梦有多强吧！";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
-			}
-			system("cls");
+	else {
+		cout << Npc(NpcId).getNpcName() << ":";
+		text = "你是谁？你要和我战斗吗？好！乐意奉陪！";
+		for (int i = 0; i < text.length(); i++)
+		{
+			Sleep(10);
+			cout << text[i];
 		}
-		else {
-			text = "亚莎：你的强大让我想起了一个人……和不同的人战斗，就能学到不同的人的优点！";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
-			}
-		}
-		break;
-	}
-	case 3: {
-		if (bag->showGoodNum(2) <= 0) {
-			text = "千里：嗯……看来，你已经赢得两枚徽章了。我真高兴能和自己的孩子来一场真真正正的战斗。但是，战斗就是战斗！身为道馆馆主，我会尽我所能赢取胜利。";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
-			}
-			cout << endl;
-			text = "你：好的老爸！可别被我打得满地找牙！";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
-			}
-			system("cls");
-		}
-		else {
-			text = "千里：你的梦想是什么？我的梦想……哈哈哈……已经实现了。";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
-			}
-		}
-		break;
-	}
-	case 4: {
-		if (bag->showGoodNum(3) <= 0) {
-			text = "米可莉：请容我正式介绍我自己。我是米可莉，琉璃道馆的道馆馆主。现在，把你和你的宝可梦的力量展现给我看吧.";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
-			}
-			cout << endl;
-			text = "你：请容我正式介绍我自己。我是秦始皇，我是来将你击败的！";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
-			}
-			system("cls");
-		}
-		else {
-			text = "米可莉：太棒了！你真的是太棒了！你是一个真正的宝可梦训练家。我为能遇见你和你的宝可梦而喜悦。";
-			for (int i = 0; i < text.length(); i++)
-			{
-				Sleep(10);
-				cout << text[i];
-			}
-		}
-		break;
-	}
-		  break;
 	}
 }
 

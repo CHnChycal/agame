f793c2a42b56577f4ccee4f8cf0d731a9ea449c7
戬 姜 <3222511849@qq.com>
2023-09-09 23:42:33 +0800

    emmm

--- npc.h
+++ npc.h
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef NPC_H
-#define NPC_H
+#ifndef NPCTEXT_H
+#define NPCTEX_H
 
 #include<string>
 #include<iostream>
@@ -10,32 +10,28 @@
 #include"Bag.h"
 #include"Goods.h"
 #include"monsters.h"
-
 using namespace std;
 class Npc {
 public:
+	bool ifChallengeSucess(int i);
 	Npc(int id);
 	~Npc();
-	void npctalk(int currentLocationIndex);			//与npc交谈并进入战斗
+	void npctalk(int currentLocationIndex, Bag* bag);//与npc交谈并进入战斗
 	string getNpcName();
 	int getNpcId();
 	string getNpcDesc();
-	void MainNpc(int currentLocationIndex);			//与npc的交互
+	void MainNpc(int currentLocationIndex, Bag* bag);//与npc的交互
 	void ShowNpc(int currentLocationIndex);          //展示npc信息
 	Monster* getFirstMonster();
 	void removeFirstMoster();
 	int getMonstersCount();
 	bool WinNpc(int whichNpc);                      //判断是否战胜npc
-	void setNpc(int isdef);							//读档设置npc
-	int ReturnIsdef();								//显示是否被打败
 private:
 	string NpcName;     //npc名称
 	int NpcId;      //npc序号
 	string NpcDesc;     //npc描述
-	int isdefeated;  //npc是否被打败(1被打败/0未击败)
-	vector<Monster>Monsters;//携带宝可梦
-	vector<string>TalkDialog;//对话文本
-	vector<string>ChallengeDialog;//挑战对话文本
+	int isdefeated;  //npc是否被打败
+	vector<Monster>gymBossMonsters;
 };
 
 #endif
